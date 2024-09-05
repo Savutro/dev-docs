f0b66560d80cb45cc7131ed23d6f2454351f40a3
Savutro <fabian.afonso@bernmobil.ch>
2024-09-05 10:36:12 +0200

    fix: change deployment config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,18 +11,11 @@ const config = {
   title: 'docs.savutro.dev',
   tagline: 'Turtles are better',
   favicon: 'img/favicon.ico',
-
-  // Set the production url of your site here
   url: 'https://docs.savutro.dev',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'savutro', // Usually your GitHub org/user name.
-  projectName: 'dev-docs', // Usually your repo name.
-
+  organizationName: 'savutro', 
+  projectName: 'dev-docs', 
+  deploymentBranch: 'main',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
